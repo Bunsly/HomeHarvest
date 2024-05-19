aacd168545186ce7f855932c135d9e103eb8ee27
Zachary Hampton <69336300+ZacharyHampton@users.noreply.github.com>
2024-05-18 17:47:55 -0700

    - alt photos bug fix

--- homeharvest/utils.py
+++ homeharvest/utils.py
@@ -84,7 +84,7 @@ def process_result(result: Property) -> pd.DataFrame:
 
     description = result.description
     prop_data["primary_photo"] = description.primary_photo
-    prop_data["alt_photos"] = ", ".join(description.alt_photos)
+    prop_data["alt_photos"] = ", ".join(description.alt_photos) if description.alt_photos else None
     prop_data["style"] = description.style if type(description.style) == str else description.style.value
     prop_data["beds"] = description.beds
     prop_data["full_baths"] = description.baths_full
